b81d27a2f43567e120f56d71c93d43a6dc4755c2
Elliot Maisl <elliot.maisl@gmail.com>
2021-11-02 00:46:13 +0100

    Add max body lengths

--- api/src/comments/dto/create-comment.dto.ts
+++ api/src/comments/dto/create-comment.dto.ts
@@ -1,7 +1,14 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateCommentDto {
   @IsNotEmpty()
+  @MinLength(25)
+  @MaxLength(2000)
   @IsString()
   body: string;
 }
--- api/src/posts/dto/create-post.dto.ts
+++ api/src/posts/dto/create-post.dto.ts
@@ -8,10 +8,14 @@ import { PostTypes } from '../../shared/types/post-types.enum';
 
 export class CreatePostDto {
   @IsNotEmpty()
+  @MinLength(15)
+  @MaxLength(100)
   @IsString()
   title: string;
 
   @IsNotEmpty()
+  @MinLength(50)
+  @MaxLength(2000)
   @IsString()
   body: string;
 
