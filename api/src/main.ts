3a65a8b98b8984ebdba85a9fcdd7153959079f84
iv-stpn <iv.stpn@gmail.com>
2021-11-05 22:03:16 +0100

    create uploads folder on build

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,3 +1,5 @@
+import { promises as fs } from 'node:fs';
+import path from 'node:path';
 import { ClassSerializerInterceptor, Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
@@ -7,6 +9,17 @@ import { AppModule } from './app.module';
 import { config } from './config';
 import { ExceptionsFilter } from './shared/filters/exceptions.filter';
 import { logger } from './shared/middlewares/logger';
+import { FileKind } from './shared/types/file-kind.enum';
+
+function enumKeys<O extends object, K extends keyof O = keyof O>(obj: O): K[] {
+  return Object.keys(obj).filter(k => Number.isNaN(Number(k))) as K[];
+}
+
+async function createFileStructure(): Promise<void> {
+  const base = path.join(path.resolve('./'), 'uploads');
+  for (const value of enumKeys(FileKind))
+    await fs.mkdir(path.join(base, FileKind[value]), { recursive: true });
+}
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -27,6 +40,7 @@ async function bootstrap(): Promise<void> {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.set('trust proxy', false);
 
+  await createFileStructure();
   await app.listen(config.get('port'));
   Logger.log(`Server initialized on port ${config.get('port')}`, 'Bootstrap');
 }
