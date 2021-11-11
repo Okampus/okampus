bef20875b8772c4f675aa0851dcc3e074c9e622f
Elliot Maisl <elliot.maisl@gmail.com>
2021-11-11 14:31:17 +0100

    Add syntax highlighting to debug sql queries

--- api/src/mikro-orm.config.ts
+++ api/src/mikro-orm.config.ts
@@ -1,5 +1,6 @@
 import type { Options } from '@mikro-orm/core';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
 import { Logger } from '@nestjs/common';
 import { config } from './config';
 
@@ -13,6 +14,7 @@ export default {
   entities: ['./dist/**/*.entity.js'],
   entitiesTs: ['./src/**/*.entity.ts'],
   debug: config.get('nodeEnv') === 'development',
+  highlighter: new SqlHighlighter(),
   logger: ormLogger.log.bind(ormLogger),
   metadataProvider: TsMorphMetadataProvider,
 } as Options;
