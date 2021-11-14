b1f3a3fab6ae9059d99148d63b5520ffe92b9107
Elliot Maisl <elliot.maisl@gmail.com>
2021-11-14 20:43:20 +0100

    Fix `enumKeys`
    My mistake, sorry

--- api/src/shared/lib/utils/enumKeys.ts
+++ api/src/shared/lib/utils/enumKeys.ts
@@ -1,3 +1,3 @@
 export function enumKeys<O extends object, K extends keyof O = keyof O>(obj: O): K[] {
-  return Object.keys(obj).map(Number).filter(Number.isNaN) as K[];
+  return Object.keys(obj).filter(k => Number.isNaN(Number(k))) as K[];
 }
