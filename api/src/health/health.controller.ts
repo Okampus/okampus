50e934648f507c09ff76d468695786690cda3c2d
Elliot Maisl <elliot.maisl@gmail.com>
2022-02-11 00:01:57 +0100

    Refactor Health controller

--- api/src/health/health.controller.ts
+++ api/src/health/health.controller.ts
@@ -3,10 +3,11 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import type { HealthCheckResult, HealthIndicatorResult } from '@nestjs/terminus';
 import {
-  DiskHealthIndicator,
+ DiskHealthIndicator,
   HealthCheck,
   HealthCheckService,
   HttpHealthIndicator,
+  MemoryHealthIndicator,
 } from '@nestjs/terminus';
 import { computedConfig, config } from '../shared/configs/config';
 import { Public } from '../shared/lib/decorators/public.decorator';
@@ -25,29 +26,33 @@ export class HealthController {
     private readonly typesense: TypesenseHealthIndicator,
     private readonly storage: StorageHealthIndicator,
     private readonly disk: DiskHealthIndicator,
+    private readonly memory: MemoryHealthIndicator,
   ) {}
 
   @Get()
   @HealthCheck()
   @Public()
   public async check(): Promise<HealthCheckResult> {
-    const storageChecks = config.get('storage.enabled') ? [
-      async (): Promise<HealthIndicatorResult> => this.storage.pingCheck('storage', 'profile-images'),
-      async (): Promise<HealthIndicatorResult> => this.storage.pingCheck('storage', 'attachments'),
-      async (): Promise<HealthIndicatorResult> => this.storage.pingCheck('storage', 'documents'),
-    ] : [
-      async (): Promise<HealthIndicatorResult> => this.disk.checkStorage('disk', {
-        path: path.join(path.resolve('./'), config.get('upload.path')),
-        thresholdPercent: 0.75,
-      }),
-    ];
+    const MAX_HTTP_DELAY = 2000;
+    const MAX_HEAP_SIZE = 500 * 1024 * 1024;
+    const LOCAL_STORAGE_OPTIONS = {
+      path: path.join(path.resolve('./'), config.get('upload.path')),
+      thresholdPercent: 0.75,
+    };
 
     return await this.health.check([
-      async (): Promise<HealthIndicatorResult> => this.http.pingCheck('api', computedConfig.apiUrl),
-      async (): Promise<HealthIndicatorResult> => this.http.pingCheck('site', computedConfig.frontendUrl),
+      async (): Promise<HealthIndicatorResult> => this.http.pingCheck('api', computedConfig.apiUrl, { timeout: MAX_HTTP_DELAY }),
+      async (): Promise<HealthIndicatorResult> => this.http.pingCheck('site', computedConfig.frontendUrl, { timeout: MAX_HTTP_DELAY }),
       async (): Promise<HealthIndicatorResult> => this.database.pingCheck('database'),
       async (): Promise<HealthIndicatorResult> => this.typesense.pingCheck('search'),
-      ...storageChecks,
+      async (): Promise<HealthIndicatorResult> => this.memory.checkHeap('memory', MAX_HEAP_SIZE),
+      ...(config.get('storage.enabled') ? [
+        async (): Promise<HealthIndicatorResult> => this.storage.pingCheck('storage', 'profile-images'),
+        async (): Promise<HealthIndicatorResult> => this.storage.pingCheck('storage', 'attachments'),
+        async (): Promise<HealthIndicatorResult> => this.storage.pingCheck('storage', 'documents'),
+      ] : [
+        async (): Promise<HealthIndicatorResult> => this.disk.checkStorage('disk', LOCAL_STORAGE_OPTIONS),
+      ]),
     ]);
   }
 }
