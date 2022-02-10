dbec7bf865d0ca5e6345ff6af59aca2a757e75ff
iv-stpn <iv.stpn@gmail.com>
2022-02-10 05:00:49 +0100

    ThreadPreviewCard: removed extra margin, don't show lastUpdated date if same as createdAt date, slightly responsive design & minor fixes

--- site/src/components/App/Card/ThreadPreviewCard.vue
+++ site/src/components/App/Card/ThreadPreviewCard.vue
@@ -1,63 +1,64 @@
 <template>
-    <div class="mb-4">
-        <div v-if="thread" class="w-full rounded-lg rounded-l-xl bg-0">
-            <div class="py-3 px-5 w-full">
-                <span
-                    class="flex overflow-hidden flex-wrap gap-5 items-center h-8 font-light whitespace-nowrap text-3"
+    <div v-if="thread" class="w-full xs:rounded-xl bg-0">
+        <div class="py-2 px-4 w-full md:py-3 md:px-5">
+            <span
+                class="flex overflow-hidden flex-wrap gap-5 items-center h-8 font-light whitespace-nowrap text-3"
+            >
+                <AppTag
+                    :icon="postTypesEnum[thread.type]?.icon"
+                    :tag-color="postTypesEnum[thread.type]?.color"
+                    :tag-name="postTypesEnum[thread.type][$i18n.locale]"
+                />
+                <div :class="[thread.solved ? 'text-green-600' : 'text-red-500']">
+                    {{ thread.solved ? 'Résolu' : 'Non-Résolu' }}
+                </div>
+                <div class="flex gap-2 items-center pl-1">
+                    <font-awesome-icon icon="calendar" />
+                    <div>{{ timeAgo(thread.createdAt) }}</div>
+                </div>
+                <div
+                    v-if="thread.createdAt !== thread.post.contentLastUpdatedAt"
+                    class="flex gap-2 items-center pl-1"
                 >
-                    <AppTag
-                        :icon="postTypesEnum[thread.type]?.icon"
-                        :tag-color="postTypesEnum[thread.type]?.color"
-                        :tag-name="postTypesEnum[thread.type][$i18n.locale]"
+                    <font-awesome-icon icon="history" />
+                    <div>{{ timeAgo(thread.post.contentLastUpdatedAt) }}</div>
+                </div>
+            </span>
+
+            <div class="mt-2">
+                <router-link
+                    :to="`/posts/${thread.contentMasterId}`"
+                    class="text-xl font-semibold hover:underline line-clamp-1 text-0"
+                >
+                    {{ thread.title }}
+                </router-link>
+
+                <p class="mt-1 text-justify line-clamp-2 text-2">
+                    {{ extractTextFromTipTapJSON(JSON.parse(thread.post.body)) }}
+                </p>
+            </div>
+
+            <div class="flex items-center mt-2 mr-4 space-x-2 h-12">
+                <a href="#" class="flex shrink-0 items-center mr-4">
+                    <UserPreview
+                        :username="thread.post.author?.fullname ?? 'Anonyme'"
+                        :img-size="12"
+                        :avatar="thread.post.author?.avatar"
+                        :reputation="thread.post.author?.points ?? 0"
                     />
-                    <div :class="[thread.solved ? 'text-green-600' : 'text-red-500']">
-                        {{ thread.solved ? 'Résolu' : 'Non-Résolu' }}
-                    </div>
-                    <div class="flex gap-2 items-center pl-1">
-                        <font-awesome-icon icon="calendar" />
-                        <div>{{ timeAgo(thread.createdAt) }}</div>
-                    </div>
-                    <div class="flex gap-2 items-center pl-1">
-                        <font-awesome-icon icon="history" />
-                        <div>{{ timeAgo(thread.post.contentLastUpdatedAt) }}</div>
-                    </div>
-                </span>
+                </a>
 
-                <div class="mt-2">
-                    <router-link
-                        :to="`/posts/${thread.contentMasterId}`"
-                        class="text-xl font-semibold hover:underline line-clamp-1 text-0"
-                    >
-                        {{ thread.title }}
-                    </router-link>
-
-                    <p class="mt-1 text-justify line-clamp-2 text-2">
-                        {{ extractTextFromTipTapJSON(JSON.parse(thread.post.body)) }}
-                    </p>
-                </div>
-
-                <div class="flex items-center mt-2 mr-4 space-x-2 h-12">
-                    <a href="#" class="flex shrink-0 items-center mr-4">
-                        <UserPreview
-                            :username="thread.post.author?.fullname ?? 'Anonyme'"
-                            :img-size="12"
-                            :avatar="thread.post.author?.avatar"
-                            :reputation="thread.post.author?.reputation ?? 0"
-                        />
-                    </a>
-
-                    <TagsList class="w-full" :tags="thread.tags" />
-                </div>
+                <TagsList class="w-full" :tags="thread.tags" />
             </div>
         </div>
-        <div v-else class="flex py-3 px-5 space-x-2 font-semibold rounded-lg bg-0">
-            <p class="text-lg text-0">Erreur: Ce post est vide.</p>
+    </div>
+    <div v-else class="flex py-3 px-5 space-x-2 font-semibold rounded-lg bg-0">
+        <p class="text-lg text-0">Erreur: Ce post est vide.</p>
 
-            <!-- TODO: Bug report pages -->
-            <router-link :to="`/report-bug/posts`" class="text-lg font-semibold line-clamp-1 link-blue">
-                Signalez ce bug !
-            </router-link>
-        </div>
+        <!-- TODO: Bug report pages -->
+        <router-link :to="`/report-bug/posts`" class="text-lg font-semibold line-clamp-1 link-blue">
+            Signalez ce bug !
+        </router-link>
     </div>
 </template>
 
