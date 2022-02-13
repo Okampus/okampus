e060fede78f9a1429aeb4dd224f4e1e61c684bd2
iv-stpn <iv.stpn@gmail.com>
2022-02-13 22:24:02 +0100

    added min-height to abstract view pages (as to take actual space where the absolute gradient banner is positioned)

--- site/src/views/App/CardPage.vue
+++ site/src/views/App/CardPage.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="absolute top-0 left-0 py-12 w-full h-52 hero" />
         <div
-            class="flex relative flex-col mt-8 mb-6 space-y-4 rounded md:mx-auto md:mt-12 md:mb-10 md:w-11/12 md:rounded-lg card"
+            class="flex relative flex-col mt-8 space-y-4 min-h-[9.5rem] rounded md:mx-auto md:mt-12 md:w-11/12 md:rounded-lg card bg-content"
         >
             <slot />
         </div>
--- site/src/views/App/ListPage.vue
+++ site/src/views/App/ListPage.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="absolute top-0 left-0 py-12 w-full h-52 hero" />
         <div
-            class="flex relative flex-col gap-2 mt-8 mb-6 w-full xs:mx-auto xs:w-22/24 md:gap-4 md:mt-12 md:mb-10 md:w-18/24 lg:w-14/24 2xl:w-1/2"
+            class="flex relative flex-col gap-2 mt-8 mb-6 w-full min-h-[9.5rem] xs:mx-auto xs:w-22/24 md:gap-4 md:mt-12 md:mb-10 md:w-18/24 lg:w-14/24 2xl:w-1/2"
         >
             <slot />
         </div>
