c7dc48a98029f96700b5befc7c2e0faafa047816
Elliot Maisl <elliot.maisl@gmail.com>
2022-02-19 22:22:38 +0100

    Add comments for myself in the repository

--- api/src/shared/lib/repositories/base.repository.ts
+++ api/src/shared/lib/repositories/base.repository.ts
@@ -16,6 +16,7 @@ export class BaseRepository<T extends BaseEntity> extends EntityRepository<T> {
     }
 
     const limit = paginationOptions.itemsPerPage ?? 10;
+    // FIXME: Do not use offset
     const offset = (paginationOptions.page - 1) * limit;
 
     const [items, total] = await this.findAndCount(
@@ -27,8 +28,8 @@ export class BaseRepository<T extends BaseEntity> extends EntityRepository<T> {
   }
 
   // TODO: findOneOrCreate — find the document, or create it
-  // TODO: createOrFail — create the document, or fails if it already exists
-  // TODO: removeOrFail — remove the document, or fails if it doesn't exist
-  // TODO: findOneAndUpdate — find the document, or fails if it doesn't exist
-  // TODO: findOneAndUpdateOrFail — find the document, or fails if it doesn't exist
+  // TODO: createOrFail — create the document, or fail if it already exists
+  // TODO: removeOrFail — remove the document, or fail if it doesn't exist
+  // TODO: findOneAndUpdate — find a document and update it
+  // TODO: findOneAndUpdateOrFail — find the document, or fail if it doesn't exist, and update it
 }
