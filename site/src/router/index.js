657595e5325c291f0d0df8108ec427be5f315f22
iv-stpn <iv.stpn@gmail.com>
2022-02-23 04:59:54 +0100

    refactored routing & error redirection approach

--- site/src/router/index.js
+++ site/src/router/index.js
@@ -1,14 +1,22 @@
+import { emitter } from '@/shared/modules/emitter'
+import { useAuthStore } from '@/store/auth.store'
 import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 const routes = [
     {
         path: '/',
         component: () => import('@/views/App/LandingPage.vue'),
+        meta: {
+            requiresAuth: false,
+        },
     },
 
     {
-        path: '/admin/:component',
+        path: '/admin/:tab',
         component: () => import('@/views/Dashboard/DashboardAdmin.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
@@ -19,71 +27,125 @@ const routes = [
     {
         path: '/articles',
         component: () => import('@/views/Blog/BlogList.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
         path: '/articles/new',
         component: () => import('@/views/Blog/BlogNew.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
         path: '/articles/:id',
         component: () => import('@/views/Blog/BlogShow.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
         path: '/articles/admin',
         component: () => import('@/views/Blog/BlogAdmin.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
-        path: '/posts',
-        component: () => import('@/views/Thread/ThreadList.vue'),
+        path: '/threads',
+        component: () => import('@/views/Thread/ThreadHome.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
-        path: '/posts/new',
+        path: '/threads/new',
         component: () => import('@/views/Thread/ThreadNew.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
-        path: '/posts/:id',
-        component: () => import('@/views/Thread/ThreadCompactView.vue'),
+        name: 'threads',
+        path: '/threads/:id',
+        component: () => import('@/views/Thread/ThreadView.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
         path: '/docs',
         component: () => import('@/views/Document/DocumentList.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
         path: '/docs/new',
         component: () => import('@/views/Document/DocumentNew.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
         path: '/me/favorites',
         component: () => import('@/views/User/UserFavoriteList.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
         path: '/me/:component',
         component: () => import('@/views/User/UserMyProfile.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
         path: '/users/:userId',
         component: () => import('@/views/User/UserProfile.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
         path: '/users',
         component: () => import('@/views/User/UserList.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     {
         path: '/crous',
         component: () => import('@/views/Blog/BlogCrous.vue'),
+        meta: {
+            requiresAuth: true,
+        },
+    },
+
+    {
+        path: '/:pathMatch(.*)*',
+        component: () => import('@/views/App/AppException.vue'),
+        props: {
+            code: '404',
+        },
+        meta: {
+            requiresAuth: true,
+        },
     },
 ]
 
@@ -91,12 +153,22 @@ if (import.meta.env.DEV) {
     routes.push({
         path: '/test',
         component: () => import('@/views/App/TestPage.vue'),
+        meta: {
+            requiresAuth: true,
+        },
     })
 }
 
 const router = createRouter({
     history: import.meta.env.DEV ? createWebHashHistory() : createWebHistory(),
     routes,
+})
+
+router.afterEach((to) => {
+    const auth = useAuthStore()
+    if (to.meta?.requiresAuth && !auth.loggedIn) {
+        emitter.emit('error-route', { code: '401', path: to.path })
+    }
 })
 
 export default router
---
+++ site/src/views/App/AppException.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="flex flex-col items-center m-20 text-0">
+        <router-link to="/" class="mb-10 w-fit">
+            <div class="w-[5.5rem] h-[5.5rem] logo" />
+        </router-link>
+        <div class="text-2xl text-blue-700 uppercase">{{ props.code }}</div>
+
+        <div class="text-5xl text-center">
+            <template v-if="props.code == '404'">Cette ressources n'existe pas.</template>
+            <template v-else-if="props.code == '403'"
+                >Seuls les utilisateurs autorisés peuvent accéder à cette page.</template
+            >
+            <template v-else-if="props.code == '401'"
+                >Seuls les utilisateurs connectés peuvent accéder à cette page.</template
+            >
+            <!-- TODO: check API health -->
+            <template v-else-if="props.code == 'Erreur Réseau' && online"
+                >L'API <a class="link-blue" href="api.horizon-efrei.fr">api.horizon-efrei.fr</a> n'est pas
+                accessible depuis ce réseau.</template
+            >
+            <template v-else-if="props.code == 'Erreur Réseau' && !online"
+                >Ce navigateur n'est pas connecté à Internet.</template
+            >
+            <template v-else>Une erreur inattendue est survenue.</template>
+        </div>
+
+        <router-link to="/" class="flex gap-4 items-center mt-20 text-3xl text-blue-600 link-blue">
+            <span>Revenir à l'accueil</span> <font-awesome-icon class="text-lg" icon="arrow-right" />
+        </router-link>
+    </div>
+</template>
+
+<script setup>
+    import { onMounted, onUnmounted, ref } from 'vue'
+
+    const online = ref(navigator.onLine)
+    onMounted(() => {
+        window.addEventListener('offline', () => (online.value = false))
+        window.addEventListener('online', () => (online.value = true))
+    })
+
+    onUnmounted(() => {
+        window.removeEventListener('offline', () => (online.value = false))
+        window.removeEventListener('online', () => (online.value = true))
+    })
+
+    const props = defineProps({
+        code: {
+            type: String,
+            required: true,
+        },
+    })
+</script>
