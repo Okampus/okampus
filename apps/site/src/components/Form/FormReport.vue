731bbcddf62feb4b9185ccd8e237d57c7bf7f53b
iv-stpn <iv.stpn@gmail.com>
2022-05-11 00:37:07 +0200

    fix(reports): editor -> textarea, fix validation

--- apps/site/src/components/Form/FormReport.vue
+++ apps/site/src/components/Form/FormReport.vue
@@ -6,24 +6,19 @@
                 <div class="label-title">Raison</div>
 
                 <!-- TODO: Change editor to basic textarea ? -->
-                <MdEditor
-                    v-model="state.reason"
-                    uid="new-report"
-                    :char-count="1000"
-                    placeholder="Décris la raison de ton signalement..."
-                    @input="v$.reason.$touch"
-                >
-                    <template #error>
-                        <AppError
-                            v-if="v$.reason.$error"
-                            error="La raison de ton signalement doit faire entre 10 et 1000 caractères."
-                        />
-                    </template>
-                </MdEditor>
+                <textarea v-model="state.reason" class="input" rows="10" @input="v$.reason.$touch" />
+                <AppError
+                    v-if="v$.reason.$error"
+                    error="La raison de ton signalement doit faire entre 20 et 1000 caractères."
+                />
                 <button
                     class="font-bold text-blue-500"
                     @click="
                         () => {
+                            if (v$.$invalid) {
+                                v$.$touch()
+                                return
+                            }
                             submit()
                             close()
                         }
@@ -39,13 +34,14 @@
 <script setup>
     import AppModal from '@/components/App/AppModal.vue'
     import AppError from '@/components/App/AppError.vue'
-    import MdEditor from '@/components/App/Editor/MdEditor.vue'
 
     import { NOOP } from '@vue/shared'
     import useVuelidate from '@vuelidate/core'
     import { reactive } from 'vue'
 
     import { useReportsStore } from '@/store/reports.store'
+
+    import { maxLength, minLength, required } from '@vuelidate/validators'
 
     const props = defineProps({
         showReport: {
@@ -63,9 +59,9 @@
     const state = reactive({ reason: '' })
     const rules = {
         reason: {
-            required: true,
-            minlength: 20,
-            maxlength: 1000,
+            required,
+            minLength: minLength(20),
+            maxLength: maxLength(1000),
         },
     }
     const v$ = useVuelidate(rules, state)
