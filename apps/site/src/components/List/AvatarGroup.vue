d80e47a1d6275e380ecf8dfd0c9a942e8371b639
Blyzi <flash.spaces@gmail.com>
2022-05-16 12:35:36 +0200

    feat(component): Avatar group

---
+++ apps/site/src/components/List/AvatarGroup.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex gap-4 items-center">
+        <div class="flex" :class="{ 'gap-1': !compact }">
+            <div v-for="(_, i) in Array(iconNumber)" :key="i" :class="{ '-m-2': compact }">
+                <UserAvatar :username="users[i].fullname" :size="iconSize" />
+            </div>
+        </div>
+
+        <span v-if="users.length - iconNumber > 0" class="truncate"
+            >+ {{ users.length - iconNumber }} utilisateurs</span
+        >
+    </div>
+</template>
+
+<script setup>
+    import UserAvatar from '../User/UserAvatar.vue'
+    defineProps({
+        users: { type: Array, required: true },
+        iconNumber: { type: Number, default: 3 },
+        iconSize: { type: Number, default: 3 },
+        compact: { type: Boolean, default: true },
+    })
+</script>
