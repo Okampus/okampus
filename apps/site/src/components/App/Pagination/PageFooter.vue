7a818c149b75cc7951cb5a34dc22c954396594a7
iv-stpn <iv.stpn@gmail.com>
2022-05-20 13:52:49 +0200

    feat(footer): refactored PageFooter

--- apps/site/src/components/App/Pagination/PageFooter.vue
+++ apps/site/src/components/App/Pagination/PageFooter.vue
@@ -1,105 +1,64 @@
 <template>
-    <div
-        v-if="totalItemCount > 0"
-        class="flex sticky bottom-0 justify-between items-center py-3 px-4 md:block md:bottom-[unset] md:px-6"
-    >
-        <div class="flex flex-1 justify-between md:hidden">
-            <router-link
-                :to="`${baseRoute}?page=${currentPage - 1}`"
-                class="inline-flex relative justify-center items-center py-2 px-4 w-28 text-sm font-medium rounded-md text-0 bg-2"
-            >
-                Précédent
-            </router-link>
+    <div v-if="totalItemCount > 0" class="flex gap-3 justify-center items-center py-3 px-6">
+        <i
+            class="p-3 hover:bg-4-light hover:dark:bg-4-dark rounded-md cursor-pointer fa fa-chevron-left text-0"
+            @click="currentPage > 1 && router.push(`${baseRoute}?page=${currentPage - 1}`)"
+        />
+        <template v-for="(page, i) in pages" :key="i">
             <div
-                class="py-2 px-3 h-10 text-lg leading-tight text-center border border-opacity/40 bg-4 text-0 border-color-2"
+                v-if="page === '...'"
+                class="flex justify-center items-center h-10 text-center text-0"
+                @click="appearInput(i)"
             >
-                {{ currentPage }}/{{ totalPages }}
+                <input
+                    v-if="currentInput === i"
+                    ref="inputRef"
+                    v-model="pageInput"
+                    class="w-10 input"
+                    @blur="
+                        () => {
+                            currentInput = null
+                            pageInput = null
+                        }
+                    "
+                    @keydown="
+                        (e) => {
+                            if (
+                                e.key == 'Enter' &&
+                                isNumeric(pageInput) &&
+                                parseInt(pageInput) <= totalPages
+                            ) {
+                                router.push(`${props.baseRoute}?page=${parseInt(pageInput)}`)
+                                currentInput = null
+                                pageInput = null
+                            }
+                        }
+                    "
+                />
+                <p v-else class="w-10">...</p>
             </div>
             <router-link
-                :to="`${baseRoute}?page=${currentPage + 1}`"
-                class="inline-flex relative justify-center items-center py-2 px-4 w-28 text-sm font-medium rounded-md text-0 bg-2"
+                v-else
+                :to="`${baseRoute}?page=${page}`"
+                class="py-2 w-10 h-10 text-lg leading-tight text-center rounded-md text-0"
+                :class="[page === currentPage ? 'bg-blue-500' : 'hover:bg-3-light dark:hover:bg-3-dark']"
+                >{{ page }}</router-link
             >
-                Suivant
-            </router-link>
-        </div>
-
-        <div class="hidden sm:items-center md:flex md:flex-1 md:justify-between">
-            <div>
-                <p class="flex gap-1 text-sm text-2">
-                    <span class="font-medium text-0">{{ totalItemCount }}</span>
-                    <span>résultat{{ totalItemCount > 1 ? 's' : '' }}</span>
-                </p>
-            </div>
-
-            <div>
-                <nav
-                    class="inline-flex relative z-0 gap-2 items-center rounded-md shadow-sm"
-                    aria-label="Pagination"
-                >
-                    <span :class="{ 'cursor-not-allowed': currentPage === 1 }">
-                        <router-link
-                            :to="`${baseRoute}?page=${currentPage - 1}`"
-                            :class="[
-                                currentPage === 1
-                                    ? 'pointer-events-none text-4'
-                                    : 'hover:bg-3-light dark:hover:bg-3-dark text-0',
-                            ]"
-                            class="inline-flex relative items-center p-2 h-10 text-sm font-medium rounded-md"
-                        >
-                            <span class="sr-only">Précédent</span>
-
-                            <i class="w-5 h-5 fas fa-chevron-left" />
-                        </router-link>
-                    </span>
-
-                    <!-- Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" -->
-                    <span class="flex gap-1 items-center">
-                        <template v-for="(page, i) in pages" :key="i">
-                            <div
-                                v-if="page === '...'"
-                                class="flex justify-center items-center w-10 h-10 text-0"
-                            >
-                                ...
-                            </div>
-                            <router-link
-                                v-else
-                                :to="`${baseRoute}?page=${page}`"
-                                class="py-2 w-10 h-10 text-lg leading-tight text-center rounded-md text-0"
-                                :class="[
-                                    page === currentPage
-                                        ? 'bg-2-light dark:bg-2-dark '
-                                        : 'hover:bg-3-light dark:hover:bg-3-dark',
-                                ]"
-                                >{{ page }}</router-link
-                            >
-                        </template>
-                    </span>
-
-                    <span :class="{ 'cursor-not-allowed': currentPage === totalPages }">
-                        <router-link
-                            :to="`${baseRoute}?page=${currentPage + 1}`"
-                            class="inline-flex relative items-center p-2 h-10 text-sm font-medium hover:bg-3-light dark:hover:bg-3-dark rounded-md text-0"
-                            :class="[
-                                currentPage === totalPages
-                                    ? 'pointer-events-none text-4'
-                                    : 'hover:bg-3-light dark:hover:bg-3-dark text-0',
-                            ]"
-                        >
-                            <span class="sr-only">Suivant</span>
-                            <i class="w-5 h-5 fas fa-chevron-right" />
-                        </router-link>
-                    </span>
-                </nav>
-            </div>
-
-            <div></div>
-        </div>
+        </template>
+        <i
+            class="p-3 hover:bg-4-light hover:dark:bg-4-dark rounded-md cursor-pointer fa fa-chevron-right text-0"
+            @click="currentPage < totalPages && router.push(`${baseRoute}?page=${currentPage + 1}`)"
+        />
     </div>
 </template>
 
 <script setup>
     import { range } from 'lodash'
-    import { computed } from 'vue'
+    import { computed, ref, nextTick } from 'vue'
+    import { useRouter } from 'vue-router'
+
+    import { isNumeric } from '@/utils/stringUtils'
+
     const numberPagesShownAround = 2
     const maxPagesShown = numberPagesShownAround * 2 + 1
 
@@ -126,12 +85,25 @@
         },
     })
 
+    const currentInput = ref(null)
+    const pageInput = ref('')
+    const inputRef = ref(null)
+
+    const router = useRouter()
+
+    const appearInput = (i) => {
+        currentInput.value = i
+        nextTick(() => {
+            inputRef.value[0].focus()
+        })
+    }
+
     const getPageNumbers = (currentPage, totalPages) => {
-        if (currentPage < numberPagesShownAround + 1) {
+        if (currentPage < maxPagesShown - 1) {
             return [...range(1, maxPagesShown), '...', totalPages]
         }
 
-        if (currentPage > totalPages - numberPagesShownAround) {
+        if (currentPage > totalPages - maxPagesShown + 2) {
             return [1, '...', ...range(totalPages - maxPagesShown + 2, totalPages + 1)]
         }
 
--- apps/site/src/components/Bar/FooterBar.vue
+++ apps/site/src/components/Bar/FooterBar.vue
@@ -1,87 +1,5 @@
 <template>
     <footer class="flex flex-col gap-6 mt-8 mb-6 xs:mx-20 sm:mx-auto xs-max:mx-10">
-        <div class="flex flex-col items-center sm:gap-6 xl:flex-row xl:gap-2">
-            <div class="flex flex-col gap-6 sm-max:w-full">
-                <!-- Logo Separator -->
-                <AppLogo class="ml-5" />
-
-                <div
-                    class="flex flex-col-reverse gap-6 mx-10 lg:flex-row lg:gap-14 sm-max:flex-row sm-max:gap-12"
-                >
-                    <!-- Contact Us -->
-                    <div class="flex gap-4 mt-1 text-0">
-                        <div
-                            class="flex flex-row justify-between sm:gap-2.5 sm:items-center sm:mr-14 sm:ml-6 sm:w-full lg:flex-col lg:mx-0 lg:w-fit sm-max:flex-col sm-max:mb-10 sm-max:ml-2"
-                        >
-                            <a
-                                v-for="(link, i) in contacts"
-                                :key="i"
-                                class="flex text-3xl lg:h-5 lg:text-xl flex-start"
-                                :href="link.href"
-                            >
-                                <i :class="link.icon" class="hover-icon" />
-                            </a>
-                        </div>
-                        <div class="hidden flex-col gap-2.5 lg:flex">
-                            <a
-                                v-for="(link, i) in contacts"
-                                :key="i"
-                                :href="link.href"
-                                target="_blank"
-                                class="h-5 text-base link"
-                                >{{ link.label }}</a
-                            >
-                        </div>
-                    </div>
-
-                    <!-- Sections -->
-                    <div
-                        class="flex flex-wrap items-center sm:flex-row sm:gap-10 sm:items-start sm-max:w-full"
-                    >
-                        <div
-                            v-for="(section, i) in footerSections"
-                            :key="i"
-                            class="sm-max:mb-10 sm-max:w-1/2 text-1"
-                        >
-                            <h5 class="mb-2 text-base tracking-wider uppercase header text-0">
-                                {{ section.name }}
-                            </h5>
-                            <div class="flex flex-col gap-1">
-                                <div v-for="(link, j) in section.links" :key="j">
-                                    <AppLink :link="link" class="text-base link" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Discord Invite Previews -->
-            <div class="flex flex-col gap-2 w-full lg:flex-row lg:w-fit xl:flex-col">
-                <GuildPreviewCard
-                    v-for="(server, i) in servers"
-                    :key="i"
-                    class="hidden lg:flex"
-                    :name="server.name"
-                    :icon-url="server.iconUrl"
-                    :invite-url="server.inviteUrl"
-                    :description="server.description"
-                    :presence="server.presence"
-                />
-                <GuildPreviewCard
-                    v-for="(server, i) in servers"
-                    :key="i"
-                    class="hidden lg-max:flex"
-                    :mini="false"
-                    :name="server.name"
-                    :icon-url="server.iconUrl"
-                    :invite-url="server.inviteUrl"
-                    :description="server.description"
-                    :presence="server.presence"
-                />
-            </div>
-        </div>
-
         <!-- Copyright -->
         <div class="flex justify-center items-center text-0">
             <div class="flex flex-col gap-2 items-center xs:flex-row">
@@ -96,16 +14,6 @@
         </div>
     </footer>
 </template>
-
-<script setup>
-    import AppLink from '../App/AppLink.vue'
-    import AppLogo from '../App/AppLogo.vue'
-    import GuildPreviewCard from '../App/Card/GuildPreviewCard.vue'
-
-    import servers from '@/shared/navigation/discord-servers.enum'
-    import contacts from '@/shared/navigation/footer-contacts.enum'
-    import footerSections from '@/shared/navigation/footer-sections.enum'
-</script>
 
 <style lang="scss">
     .hover-icon {
