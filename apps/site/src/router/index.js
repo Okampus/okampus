c9ead602ee254657b6f3b52fe6f5a296d8efdf42
iv-stpn <iv.stpn@gmail.com>
2022-05-25 01:41:18 +0200

    refactor(DashboardAssocation): rename DashboardClub

--- apps/site/src/router/index.js
+++ apps/site/src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
 
     {
         path: '/admin/clubs',
-        component: () => import('@/views/Dashboard/DashboardAssociation.vue'),
+        component: () => import('@/views/Dashboard/DashboardClub.vue'),
         meta: {
             requiresAuth: true,
         },
