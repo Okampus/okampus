e0cf2e1af1585b5cda864218e694ae8b07465b9d
iv-stpn <iv.stpn@gmail.com>
2022-05-25 01:23:27 +0200

    feat(enum): add resource-names.enum

---
+++ apps/site/src/shared/types/resource-names.enum.js
@@ -0,0 +1,33 @@
+export const DEFAULT = 'resource'
+export const THREAD = 'thread'
+export const USER = 'user'
+export const CLUB = 'club'
+export const EVENT = 'event'
+
+export const RESOURCE_NAMES = {
+    [DEFAULT]: {
+        demonstrative: { fr: 'cette ressource', en: 'this resource' },
+        name: { fr: 'ressource', en: 'resource' },
+        frFeminine: true,
+    },
+    [THREAD]: {
+        demonstrative: { fr: 'ce post', en: 'this post' },
+        name: { fr: 'post', en: 'post' },
+        frFeminine: false,
+    },
+    [USER]: {
+        demonstrative: { fr: 'cet utilisateur', en: 'this user' },
+        name: { fr: 'utilisateur', en: 'user' },
+        frFeminine: false,
+    },
+    [CLUB]: {
+        demonstrative: { fr: 'cette association', en: 'this club' },
+        name: { fr: 'association', en: 'club' },
+        frFeminine: true,
+    },
+    [EVENT]: {
+        demonstrative: { fr: 'cet événement', en: 'this event' },
+        name: { fr: 'événement', en: 'event' },
+        frFeminine: false,
+    },
+}
