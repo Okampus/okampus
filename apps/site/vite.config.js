20878b83f4f0445405a972679e1f7c20ac79651a
Théo <flash.spaces@gmail.com>
2022-07-09 00:50:12 +0200

    add PWA to webapp (#7)

--- apps/site/vite.config.js
+++ apps/site/vite.config.js
@@ -1,9 +1,48 @@
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        VitePWA({
+            mode: import.meta.env,
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+            registerType: 'autoUpdate',
+            strategies: 'injectManifest',
+            srcDir: resolve(__dirname, 'src'),
+            filename: 'sw.js',
+            base: '/',
+            manifest: {
+                name: 'Okampus',
+                short_name: 'Okampus',
+                description: 'Votre campus en ligne',
+                theme_color: '#1e293b',
+                icons: [
+                    {
+                        src: 'icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+            devOptions: {
+                enabled: true,
+                type: 'module',
+                autoUpdate: {
+                    enabled: true,
+                    interval: 60,
+                    retry: 3,
+                },
+            },
+        }),
+    ],
     resolve: { alias: { '@': resolve(__dirname, 'src') } },
     server: { open: true },
 })
