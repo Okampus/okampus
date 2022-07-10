0317f6f79166b5eb65e43e22a064180287f52c85
iv-stpn <iv.stpn@gmail.com>
2022-07-10 23:57:33 +0200

    fix(UserList): better UserList and UserCard

--- apps/site/src/components/App/ListCard/UserCard.vue
+++ apps/site/src/components/App/ListCard/UserCard.vue
@@ -1,28 +1,29 @@
 <template>
-    <div class="flex gap-4 p-4 mx-4 xs:rounded-xl bg-content">
-        <ProfileAvatar
-            :size="4"
-            class="px-2 my-auto min-w-fit"
-            :avatar="user.avatar"
-            :name="fullname(user)"
-        />
-        <div class="flex flex-col gap-1.5">
-            <div class="flex gap-3 text-lg text-0 align-items">
-                <router-link :to="`/user/${user.userId}`" class="my-auto font-semibold hover:underline">{{
-                    fullname(user)
-                }}</router-link>
+    <div class="flex gap-4 items-start card-2">
+        <div class="flex flex-col gap-3 items-center">
+            <ProfileAvatar :size="4" class="px-2 min-w-fit" :avatar="user.avatar" :name="fullname(user)" />
+
+            <TipPopper :tip="`${user.points} points`">
+                <div class="inline-flex flex-nowrap gap-2 ml-1 w-fit text-5 align-items">
+                    <div class="text-xl">🏆</div>
+                    <div class="font-semibold text-1">{{ user.points }}</div>
+                </div>
+            </TipPopper>
+        </div>
+        <div class="flex flex-col gap-3">
+            <div class="inline-flex flex-wrap gap-x-6 text-lg text-0 align-items">
+                <router-link
+                    :to="`/user/${user.userId}`"
+                    class="inline my-auto font-semibold hover:underline"
+                    >{{ fullname(user) }}</router-link
+                >
+
                 <LabelTag
-                    class="text-sm"
+                    class="inline text-sm"
                     :tag-name="role[$i18n.locale]"
                     :tag-color="role.color"
                     :icon="role.icon"
                 />
-                <TipPopper :tip="`${user.points} points`">
-                    <div class="flex gap-2 w-fit text-5 align-items">
-                        <i class="pt-1 fas fa-trophy" />
-                        <div>{{ user.points }}</div>
-                    </div>
-                </TipPopper>
             </div>
             <div class="line-clamp-2 text-1">
                 {{ user.shortDescription }}
