62c695770c69ad97aad68c12f3c0851f63d42154
Elliot Maisl <elliot.maisl@gmail.com>
2022-07-20 20:09:24 +0200

    fix(gql): return a more helpful exception

--- apps/api/src/shared/lib/filters/exceptions.filter.ts
+++ apps/api/src/shared/lib/filters/exceptions.filter.ts
@@ -1,10 +1,14 @@
-import type { ArgumentsHost, ExceptionFilter } from '@nestjs/common';
 import { Catch, HttpException } from '@nestjs/common';
+import type { ArgumentsHost, ExceptionFilter } from '@nestjs/common';
+import type { GqlContextType } from '@nestjs/graphql';
 import type { ErrorFilterResponse } from '../types/interfaces/error-filter-response.interface';
 
 @Catch(HttpException)
 export class ExceptionsFilter implements ExceptionFilter {
   public catch(exception: HttpException, host: ArgumentsHost): ErrorFilterResponse {
+    if (host.getType<GqlContextType>() === 'graphql')
+      throw exception;
+
     const statusCode = exception.getStatus();
     const error = exception.getResponse();
 
@@ -19,12 +23,10 @@ export class ExceptionsFilter implements ExceptionFilter {
     else
       Object.assign(response, error);
 
-    if (host.getType() === 'http') {
-      host.switchToHttp()
+    host.switchToHttp()
         .getResponse()
         .status(statusCode)
         .json(response);
-    }
 
     return response;
   }
