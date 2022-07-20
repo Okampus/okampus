5c9ad51329b92cff29d7ff2bf52dd312af76c949
Elliot Maisl <elliot.maisl@gmail.com>
2022-07-20 20:08:33 +0200

    feat(logger): improve REST logger and add GQL logger

---
+++ apps/api/src/shared/lib/middlewares/rest-logger.middleware.ts
@@ -0,0 +1,18 @@
+import type { IncomingMessage, ServerResponse } from 'node:http';
+import type { NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import type { NextFunction } from 'express';
+import morgan from 'morgan';
+
+@Injectable()
+export class RestLoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('REST');
+
+  public use(req: IncomingMessage, res: ServerResponse, next: NextFunction): void {
+    morgan((tokens, req2, res2) => {
+      const lineFactory = morgan.compile(':method :url :response-time ms — :status-colored (:status-text)');
+      this.logger.log(`Request finished: ${lineFactory(tokens, req2, res2)}`);
+      return null;
+    })(req, res, next);
+  }
+}
---
+++ apps/api/src/shared/lib/morgan.register.ts
@@ -0,0 +1,16 @@
+import type { Request } from 'express';
+import morgan from 'morgan';
+import status from 'statuses';
+
+morgan.token('status-text', (_req, { statusCode }) => status(statusCode).toString());
+morgan.token('status-colored', (_req, { statusCode }) => {
+  const color = statusCode >= 500 ? 31 // Red
+    : statusCode >= 400 ? 33 // Yellow
+    : statusCode >= 300 ? 34 // Blue
+    : statusCode >= 200 ? 36 // Cyan
+    : 37; // White
+  return `\u001B[${color}m${statusCode}\u001B[32m`;
+});
+
+morgan.token('gql-op-type', (req: Request) => req.body.query.split(' ')[0]);
+morgan.token('gql-op-name', (req: Request) => req.body.operationName);
