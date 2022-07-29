80d2acdae090c6ecb724b8a7b48345e3b59a2ab9
Elliot Maisl <elliot.maisl@gmail.com>
2022-07-29 12:03:17 +0200

    feat(strapi): upload to S3

--- apps/strapi/config/plugins.ts
+++ apps/strapi/config/plugins.ts
@@ -26,4 +26,23 @@ export default ({ env }) => ({
       ],
     },
   },
+
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('S3_ACCESS_KEY_ID'),
+        secretAccessKey: env('S3_SECRET_ACCESS_KEY'),
+        endpoint: env('S3_ENDPOINT'),
+        params: {
+          Bucket: env('S3_BUCKET_NAME_STRAPI'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
