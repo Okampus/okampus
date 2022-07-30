6bfc15b6f5793b128485010c62a1eae2e21b66da
iv-stpn <iv.stpn@gmail.com>
2022-07-30 16:39:25 +0200

    fix(getRole): capitalize role to retrieve in enum

--- apps/site/src/utils/users.js
+++ apps/site/src/utils/users.js
@@ -1,5 +1,7 @@
 import { ROLES, DEFAULT_ROLE } from '@/shared/types/school-roles.enum'
+import { capitalize } from 'lodash'
 
 export const fullname = (user) =>
     user.firstname && user.lastname ? `${user.firstname.split(' ')[0]} ${user.lastname}` : 'Anonyme'
-export const getRole = (user) => ROLES.find((role) => role.key === user.schoolRole) || DEFAULT_ROLE
+export const getRole = (user) =>
+    ROLES.find((role) => role.key === capitalize(user.schoolRole)) || DEFAULT_ROLE
