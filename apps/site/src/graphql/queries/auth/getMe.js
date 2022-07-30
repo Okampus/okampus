60bb529653cf99a2ea7502f1528497ea57a913e9
iv-stpn <iv.stpn@gmail.com>
2022-07-30 18:59:43 +0200

    feat(getMe-frontend): getMe with graphql

---
+++ apps/site/src/graphql/queries/auth/getMe.js
@@ -0,0 +1,11 @@
+import { userFragment } from '@/graphql/fragments/userFragment'
+import gql from 'graphql-tag'
+
+export const getMe = gql`
+    query getMe {
+        me {
+            ...UserInfo
+        }
+    }
+    ${userFragment}
+`
--- apps/site/src/views/App/AuthPage.vue
+++ apps/site/src/views/App/AuthPage.vue
@@ -5,10 +5,32 @@
 <script setup>
     import AppLoader from '@/components/App/AppLoader.vue'
     import { useAuthStore } from '@/store/auth.store'
+    import { showToastGraphQLError } from '@/utils/errors'
+    import { useQuery } from '@vue/apollo-composable'
     import { useRouter } from 'vue-router'
 
+    import { getMe } from '@/graphql/queries/auth/getMe'
+    import { emitter } from '@/shared/modules/emitter'
+
     const router = useRouter()
-    useAuthStore()
-        .getMe()
-        .then(() => router.push('/'))
+    const { onResult, onError } = useQuery(getMe)
+
+    const auth = useAuthStore()
+
+    onResult(({ data }) => {
+        if (data.me) {
+            auth.user = data.me
+            emitter.emit('show-toast', {
+                message: 'Connexion réussie !',
+                type: 'success',
+            })
+        } else {
+            emitter.emit('show-toast', {
+                message: "[Erreur] Vos informations utilisateur n'ont pas pu être récupérées.",
+                type: 'error',
+            })
+        }
+        router.push('/')
+    })
+    onError(showToastGraphQLError)
 </script>
