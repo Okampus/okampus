0d9571db13bcac74e102d533dc20c1ed105df809
iv-stpn <iv.stpn@gmail.com>
2022-07-30 16:35:18 +0200

    feat(gql): add schoolGroup to userFragment

--- apps/site/src/graphql/fragments/userFragment.js
+++ apps/site/src/graphql/fragments/userFragment.js
@@ -1,11 +1,32 @@
 import gql from 'graphql-tag'
 
+const schoolGroupFragment = gql`
+    fragment schoolGroupFragment on SchoolGroup {
+        id
+        name
+        type
+    }
+`
+
 export const userFragment = gql`
     fragment UserInfo on User {
         id
         firstname
         lastname
         avatar
+        roles
         schoolRole
+        schoolGroupMemberships {
+            role
+            schoolYear {
+                id
+                name
+                active
+            }
+            getParents {
+                ...schoolGroupFragment
+            }
+        }
     }
+    ${schoolGroupFragment}
 `
