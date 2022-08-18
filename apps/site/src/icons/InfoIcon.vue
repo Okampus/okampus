e99a8da695816d70b679727d65090644ec52115e
iv-stpn <iv.stpn@gmail.com>
2022-08-19 01:56:59 +0200

    feat(InfoIcon): add custom InfoIcon

---
+++ apps/site/src/icons/InfoIcon.vue
@@ -0,0 +1,15 @@
+<template>
+    <i
+        v-tooltip="tooltip"
+        class="far fa-circle-question text-2 ml-2 cursor-help text-xl hover:text-blue-500"
+    />
+</template>
+
+<script setup>
+    defineProps({
+        tooltip: {
+            type: String,
+            required: true,
+        },
+    })
+</script>
