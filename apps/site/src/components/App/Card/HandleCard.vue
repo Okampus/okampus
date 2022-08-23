37de142396c428385e1e9279821ac78665df2310
iv-stpn <iv.stpn@gmail.com>
2022-08-23 02:51:46 +0200

    feat(HandleCard): refactor card with handles in HandleCard

---
+++ apps/site/src/components/App/Card/HandleCard.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="group card-0 flex flex-col">
+        <div
+            class="handle -mt-2 flex h-4 w-full cursor-move justify-center opacity-0 transition-opacity group-hover:opacity-100"
+        >
+            <i class="text-3 fa fa-grip" />
+        </div>
+
+        <slot />
+    </div>
+</template>
