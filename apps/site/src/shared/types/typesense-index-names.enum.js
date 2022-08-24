fb247986df9077a6aff8afdcc519fe65379e4d1b
iv-stpn <iv.stpn@gmail.com>
2022-08-24 14:52:11 +0200

    refactor(🗑️): remove typesense-index-names

--- apps/site/src/shared/types/typesense-index-names.enum.js
+++
@@ -1,8 +0,0 @@
-export const THREADS = 'threads'
-export const BLOGS = 'blogs'
-export const TEAMS = 'teams'
-export const REPORTS = 'reports'
-export const INFO_DOCS = 'info-docs'
-export const STUDY_DOCS = 'study-docs'
-export const USERS = 'users'
-export const SUBJECTS = 'subjects'
