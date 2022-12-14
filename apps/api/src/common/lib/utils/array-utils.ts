e22aae49737bbdb0b45e01c70f36c2458a8d17db
Elliot Maisl <elliot.maisl@gmail.com>
2022-12-13 19:43:27 -0500

    refactor(array-utils): simplify logic

--- apps/api/src/common/lib/utils/array-utils.ts
+++ apps/api/src/common/lib/utils/array-utils.ts
@@ -1,38 +1,22 @@
-export function sampleArray<T>(arr: T[], min: number, max?: number): T[] {
+export function sampleArrayWithRest<T>(arr: T[], min: number, max = 0): [res: T[], rest: T[]] {
   arr = [...arr];
   const len = arr.length;
-  const size = max ? Math.floor(Math.random() * (max - min + 1)) + min : min;
-  const res = [];
-  for (let i = 0; i < size; i++) {
-      const j = Math.floor(Math.random() * (len - i));
-      res.push(arr[j]);
-      arr[j] = arr[len - i - 1];
-  }
-  return res;
-}
+  const size = Math.floor(Math.random() * (max - min + 1)) + min;
+  const res: T[] = [];
 
-export function sampleArrayWithRest<T>(arr: T[], min: number, max?: number): [res: T[], rest: T[]] {
-  arr = [...arr];
-  const len = arr.length;
-  const size = max ? Math.floor(Math.random() * (max - min + 1)) + min : min;
-  const res = [];
   for (let i = 0; i < size; i++) {
-      const j = Math.floor(Math.random() * (len - i));
-      res.push(arr[j]);
-      arr[j] = arr[len - i - 1];
+    const j = Math.floor(Math.random() * (len - i));
+    res.push(arr[j]);
+    arr[j] = arr[len - i - 1];
   }
+
   return [res, arr];
 }
 
+export function sampleArray<T>(arr: T[], min: number, max = 0): T[] {
+  return sampleArrayWithRest<T>(arr, min, max)[0];
+}
+
 export function shuffleArray<T>(arr: T[]): T[] {
-  let j;
-  let x;
-  let i;
-  for (i = arr.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      x = arr[i];
-      arr[i] = arr[j];
-      arr[j] = x;
-  }
-  return arr;
+  return arr.sort(() => Math.random() - 0.5);
 }
