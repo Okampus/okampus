70be4fcf142cf722db253e1593183abc5333baa5
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-02-09 23:55:36 +0100

    🐛 DTOs: fix IRole/ITeamRole "key" prop

--- libs/shared/dtos/src/resources/role/team-role/team-role.interface.ts
+++ libs/shared/dtos/src/resources/role/team-role/team-role.interface.ts
@@ -1,3 +1,4 @@
+import type { TeamRoleKey } from '@okampus/shared/enums';
 import type { ITeam } from '../../org/team/team.interface';
 import type { IRole } from '../role.interface';
 import type { TeamRoleProps } from './team-role.props';
@@ -5,4 +6,5 @@ import type { TeamRoleProps } from './team-role.props';
 export type ITeamRole = IRole &
   TeamRoleProps & {
     team?: ITeam;
+    key: TeamRoleKey | null;
   };
