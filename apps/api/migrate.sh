a0c3af64e1cd4d51e03f47662453f4dfca452778
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-02-10 02:41:59 +0100

    💚 CI: don't create migrations in Dockerfile

--- apps/api/migrate.sh
+++ apps/api/migrate.sh
@@ -1,1 +1,4 @@
-(export MIKRO_ORM_MIGRATION=create && node dist/apps/api/main.mjs) && pnpm tsc --pretty -p apps/api/tsconfig.migrations.json && (export MIKRO_ORM_MIGRATION=up && node dist/apps/api/main.mjs)
+# Compile migrations to js in dist folder (migrations should be up-to-date in apps/api/database/migrations)
+pnpm tsc --pretty -p apps/api/tsconfig.migrations.json;
+# Workaround for `pnpm mikro-orm migration:up` without CLI & ts-node
+MIKRO_ORM_MIGRATION=up node dist/apps/api/main.mjs;
