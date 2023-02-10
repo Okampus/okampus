babbb64277cbb537bb200a508fd1872d335184e8
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-02-10 20:29:00 +0100

    🐛 API: don't throw error in CORS

--- apps/api/src/bootstrap/cors.validation.ts
+++ apps/api/src/bootstrap/cors.validation.ts
@@ -5,7 +5,7 @@ export const corsValidation: OriginFunction = (origin, cb) => {
   if (config.env.isProd()) {
     return /^https:\/\/(?:[\dA-Za-z][\dA-Za-z-]{1,61}[\dA-Za-z])+\.okampus\.fr(?::\d+)?/.test(origin)
       ? cb(null, true)
-      : cb(new Error('Not allowed by CORS'), false);
+      : cb(null, false);
   }
 
   cb(null, true);
