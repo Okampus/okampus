356ade74940089d33cdee84fd53f079d4f428ea0
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-02-11 23:42:50 +0100

    ✨ API: better snowflake generation

--- libs/shared/utils/src/random/random-snowflake.ts
+++ libs/shared/utils/src/random/random-snowflake.ts
@@ -1,5 +1,12 @@
-import { randomDigits } from './random-digits';
+let state = { currentMs: Date.now(), counter: 0 };
 
 export function snowflake() {
-  return `${Date.now()}${randomDigits(6)}`;
+  const now = Date.now();
+  if (now !== state.currentMs) state = { currentMs: now, counter: 0 };
+
+  const timestamp = Date.now().toString(2).padStart(42, '0'); // 42 bits - max. 140 years since 1970
+  const counter = (state.counter++).toString(2).padStart(21, '0'); // 21 bits - max. 2.1 million IDs per ms
+  const snowflake = BigInt('0b0' + timestamp + counter); // 63 bits - MySQL/Postgres positive 8 bytes bigint limit
+
+  return snowflake.toString();
 }
