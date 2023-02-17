8e42374bffa3bb73668b6f17ff960a822815f49d
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-02-17 17:27:58 +0100

    🐛 Use cross-env for running commands

--- apps/api/migrate.sh
+++ apps/api/migrate.sh
@@ -1,4 +1,4 @@
 # Compile migrations to js in dist folder (migrations should be up-to-date in apps/api/database/migrations)
 pnpm tsc --pretty -p apps/api/tsconfig.migrations.json;
 # Workaround for `pnpm mikro-orm migration:up` without CLI & ts-node
-MIKRO_ORM_MIGRATION=up node dist/apps/api/main.mjs;
+pnpm cross-env MIKRO_ORM_MIGRATION=up node dist/apps/api/main.mjs;
