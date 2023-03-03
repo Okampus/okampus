ba3beb32b391698890aa4159fa026530212f4f0c
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-03-03 14:31:51 +0100

    🐛 DTOs: fix UpdateTeamDto

--- libs/shared/dtos/src/resources/org/team/update-team.dto.ts
+++ libs/shared/dtos/src/resources/org/team/update-team.dto.ts
@@ -1,9 +1,12 @@
 import { CreateTeamDto } from './create-team.dto';
 import { Field, InputType, PartialType } from '@nestjs/graphql';
+import { IsString } from 'class-validator';
+
 import type { Snowflake } from '@okampus/shared/types';
 
 @InputType()
 export class UpdateTeamDto extends PartialType(CreateTeamDto) {
   @Field(() => String)
+  @IsString()
   id!: Snowflake;
 }
