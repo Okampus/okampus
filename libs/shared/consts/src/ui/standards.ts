8e6e0bff703b1f633ca157b739d809367ed7abf1
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-06-12 20:03:13 +0200

    ♻️ Consts: update UI size standards

--- libs/shared/consts/src/ui/standards.ts
+++ libs/shared/consts/src/ui/standards.ts
@@ -1,3 +1,5 @@
-export const BANNER_RATIO = 1 / 2.6;
+export const BANNER_RATIO = 1 / 2.5;
 export const BANNER_ASPECT_RATIO = 1 / BANNER_RATIO;
-export const SMALL_SCREEN_WIDTH = 1024;
+
+export const SMALL_SCREEN_WIDTH = 1250;
+export const XS_SCREEN_WIDTH = 896;
