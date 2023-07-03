e774bda3ca16b809dc81694059189aeab5fc6314
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-07-03 09:44:26 +0200

    🐛 Graphql: remove scopeRole from individual selector

--- libs/shared/graphql/src/selectors/individual/individualBase.ts
+++ libs/shared/graphql/src/selectors/individual/individualBase.ts
@@ -3,5 +3,5 @@ import { actorBaseInfo } from '../actor/actorBase';
 import { entityBase } from '../entityBase';
 import type { InputType, GraphQLTypes } from '../../zeus';
 
-export const individualBaseInfo = Selector('Individual')({ ...entityBase, actor: actorBaseInfo, scopeRole: true });
+export const individualBaseInfo = Selector('Individual')({ ...entityBase, actor: actorBaseInfo });
 export type IndividualBaseInfo = InputType<GraphQLTypes['Individual'], typeof individualBaseInfo>;
