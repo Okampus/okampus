35714c3885d5d9fb8f8a5d126e2c32b384c27853
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-07-24 13:39:26 +0200

    ✨ Site: add site/api urls const envs

--- apps/site/context/consts.ts
+++ apps/site/context/consts.ts
@@ -1,5 +1,12 @@
-// const isDevelopment = process.env.NODE_ENV === 'development';
-export const API_URL = 'http://localhost:8081'; //isDevelopment ? 'http://localhost:8081' : 'https://api.okampus.fr';
-export const HASURA_URL = 'http://localhost:8080'; //isDevelopment ? 'http://localhost:8081' : 'https://api.okampus.fr';
-export const HASURA_WS_URL = 'ws://localhost:8080'; //isDevelopment ? 'http://localhost:8081' : 'https://api.okampus.fr';
-export const SITE_URL = 'http://localhost:4200'; //isDevelopment ? 'http://localhost:4200' : 'https://okampus.fr';
+export const API_URL = process.env.NEXT_PUBLIC_API_DOMAIN
+  ? `https://${process.env.NEXT_PUBLIC_API_DOMAIN}`
+  : 'http://localhost:8081';
+export const HASURA_URL = process.env.NEXT_PUBLIC_HASURA_DOMAIN
+  ? `https://${process.env.NEXT_PUBLIC_HASURA_DOMAIN}`
+  : 'http://localhost:8080';
+export const HASURA_WS_URL = process.env.NEXT_PUBLIC_HASURA_DOMAIN
+  ? `ws://${process.env.NEXT_PUBLIC_HASURA_DOMAIN}`
+  : 'ws://localhost:8080';
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_DOMAIN
+  ? `https://${process.env.NEXT_PUBLIC_SITE_DOMAIN}`
+  : 'http://localhost:4200';
