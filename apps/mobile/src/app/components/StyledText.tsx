9ac90b16d209b1bfd902aa45af9e6779d6bdad16
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-07-28 19:44:35 +0200

    🔥 Mobile: remove mobile and related dependencies

--- apps/mobile/src/app/components/StyledText.tsx
+++
@@ -1,6 +0,0 @@
-import { Text } from './Themed';
-import type { TextProps } from './Themed';
-
-export function MonoText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />;
-}
--- apps/mobile/src/app/hooks/useColorScheme.ts
+++
@@ -1,9 +0,0 @@
-import { useColorScheme as _useColorScheme } from 'react-native';
-import type { ColorSchemeName } from 'react-native';
-
-// The useColorScheme value is always either light or dark, but the built-in
-// type suggests that it can be null. This will not happen in practice, so this
-// makes it a bit easier to work with.
-export default function useColorScheme(): NonNullable<ColorSchemeName> {
-  return _useColorScheme() as NonNullable<ColorSchemeName>;
-}
