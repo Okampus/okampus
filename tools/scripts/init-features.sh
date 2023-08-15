3bf78ca6e23672b7666fcb587b6472813ee58895
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-08-15 07:31:59 +0200

    ♻️ Tools: update scripts with changes

--- tools/scripts/init-features.sh
+++ tools/scripts/init-features.sh
@@ -7,7 +7,7 @@ nx g hasura-feature tag --folder $1
 nx g hasura-feature actor --folder $1
 nx g hasura-feature follow --subfolder actors --folder $1
 nx g hasura-feature social --subfolder actors --folder $1
-nx g hasura-feature bank-info --subfolder actors --folder $1
+nx g hasura-feature bank --subfolder actors --folder $1
 nx g hasura-feature legal-unit --subfolder actors --folder $1 --tenant-scoped false --expect-rels actor:name
 nx g hasura-feature legal-unit-location --subfolder actors --folder $1 --tenant-scoped false --expect-rels actor:name
 nx g hasura-feature actor-image --subfolder actors --folder $1
@@ -22,7 +22,7 @@ nx g hasura-feature team --folder $1
 nx g hasura-feature account --subfolder teams --folder $1
 nx g hasura-feature action --subfolder teams --folder $1
 nx g hasura-feature finance --subfolder teams --folder $1
-nx g hasura-feature team-join --subfolder teams --folder $1 --settles='by:processedById_at:processedAt_if:props.state === enum!ApprovalState.Rejected || (props.state === enum!ApprovalState.Approved && props.receivedRoleId)'
+nx g hasura-feature team-join --subfolder teams --folder $1 --expect-rels submission --settles='by:processedById_at:processedAt_if:props.state === enum!ApprovalState.Rejected || (props.state === enum!ApprovalState.Approved && props.receivedRoleId)'
 nx g hasura-feature mission-join --subfolder teams --folder $1 --settles='by:processedById_at:processedAt_if:props.state === enum!ApprovalState.Approved || props.state === enum!ApprovalState.Rejected;by:pointsProcessedById_at:pointsProcessedAt_if:props.points !== null'
 nx g hasura-feature tenant --folder $1 --tenant-scoped false
 nx g hasura-feature event-approval-step --subfolder tenants --folder $1
