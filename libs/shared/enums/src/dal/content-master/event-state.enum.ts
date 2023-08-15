12268db735197c704e98c240476cdc03fc6530c3
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-08-15 07:08:17 +0200

    ✨ Enums: add completed & canceled to Event state

--- libs/shared/enums/src/dal/content-master/event-state.enum.ts
+++ libs/shared/enums/src/dal/content-master/event-state.enum.ts
@@ -5,6 +5,8 @@ export enum EventState {
   Rejected = 'Rejected',
   Approved = 'Approved',
   Published = 'Published',
+  Canceled = 'Canceled',
+  Completed = 'Completed',
 }
 
 export const EVENT_STATE_COLORS = {
@@ -14,4 +16,6 @@ export const EVENT_STATE_COLORS = {
   [EventState.Rejected]: '#ff4d4f',
   [EventState.Approved]: '#52c41a',
   [EventState.Published]: '#1890ff',
+  [EventState.Canceled]: '#ff4d4f',
+  [EventState.Completed]: '#555555dd',
 };
