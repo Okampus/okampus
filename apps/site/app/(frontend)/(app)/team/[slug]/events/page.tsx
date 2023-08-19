799fce8db76dbc9b52e1d94b96d621baa72a2d37
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-08-19 16:38:56 +0200

    ✨ Site: add empty state to team events page

--- apps/site/app/(frontend)/(app)/team/[slug]/events/page.tsx
+++ apps/site/app/(frontend)/(app)/team/[slug]/events/page.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import EmptyStateImage from '../../../../../../components/atoms/Image/EmptyStateImage';
 import ViewLayout from '../../../../../../components/atoms/Layout/ViewLayout';
 import EventCard from '../../../../../../components/molecules/Card/EventCard';
 
 import { useQueryAndSubscribe } from '../../../../../../hooks/apollo/useQueryAndSubscribe';
+
+import { ReactComponent as EventsEmptyState } from '@okampus/assets/svg/empty-state/events.svg';
 
 import { GetEventsDocument, OrderBy } from '@okampus/shared/graphql';
 import type { GetEventsQuery, GetEventsQueryVariables } from '@okampus/shared/graphql';
@@ -25,11 +28,22 @@ export default function TeamEventsPage({ params }: { params: { slug: string } })
 
   return (
     <ViewLayout header="Événements">
-      <div className="mt-2 w-full grid grid-cols-[repeat(auto-fill,minmax(18rem,1fr))] gap-8">
-        {events.map((event) => (
-          <EventCard key={event.id} event={event} />
-        ))}
-      </div>
+      {events.length > 0 ? (
+        <div className="mt-2 w-full grid grid-cols-[repeat(auto-fill,minmax(18rem,1fr))] gap-8">
+          {events.map((event) => (
+            <EventCard key={event.id} event={event} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex w-full justify-center">
+          <EmptyStateImage
+            image={<EventsEmptyState />}
+            className="my-4 w-full"
+            title="Aucun événement pour le moment"
+            subtitle="Les prochains événements de l'équipe seront affichés ici."
+          />
+        </div>
+      )}
     </ViewLayout>
   );
 }
