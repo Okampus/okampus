d182876ac6338ad79af2a1932227ec4804f69b27
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-08-24 07:18:48 +0200

    ♻️ Site: use SegmentedButton in manage buttons

--- apps/site/components/layouts/SideBar/ManageButton/ProjectManageButton.tsx
+++ apps/site/components/layouts/SideBar/ManageButton/ProjectManageButton.tsx
@@ -1,27 +1,22 @@
 'use client';
 
+import SegmentedButton from '../../../../components/molecules/Button/SegmentedButton';
 import { useProject } from '../../../../context/navigation';
-import { useTranslation } from '../../../../hooks/context/useTranslation';
-
-import clsx from 'clsx';
-import Link from 'next/link';
 
 export type ProjectManageButtonProps = { slug: string; manage: boolean };
 export default function ProjectManageButton({ slug, manage }: ProjectManageButtonProps) {
   const { canManage } = useProject(slug);
-  const { t } = useTranslation();
 
   if (manage && !canManage) return null;
 
   return (
-    <Link
-      href={manage ? `/manage/project/${slug}` : `/project/${slug}`}
-      className={clsx(
-        'flex items-center justify-center h-12 w-[calc(100%-1rem)] mb-3 mx-[0.5rem] font-semibold border-2 border-color-1 rounded-md',
-        manage ? 'text-0' : 'text-opposite bg-opposite',
-      )}
-    >
-      {t(manage ? 'common.manage.project' : 'common.public.project')}
-    </Link>
+    <SegmentedButton
+      className="mx-3 mb-4"
+      initialIndex={manage ? 0 : 1}
+      options={[
+        { label: 'Vue publique', action: `/project/${slug}` },
+        { label: 'Vue gestion', action: `/manage/project/${slug}` },
+      ]}
+    />
   );
 }
--- apps/site/components/layouts/SideBar/ManageButton/TeamManageButton.tsx
+++ apps/site/components/layouts/SideBar/ManageButton/TeamManageButton.tsx
@@ -1,27 +1,22 @@
 'use client';
 
+import SegmentedButton from '../../../../components/molecules/Button/SegmentedButton';
 import { useTeam } from '../../../../context/navigation';
-import { useTranslation } from '../../../../hooks/context/useTranslation';
-
-import clsx from 'clsx';
-import Link from 'next/link';
 
 export type TeamManageButtonProps = { slug: string; manage: boolean };
 export default function TeamManageButton({ slug, manage }: TeamManageButtonProps) {
   const { canManage } = useTeam(slug);
-  const { t } = useTranslation();
 
   if (manage && !canManage) return null;
 
   return (
-    <Link
-      href={manage ? `/manage/team/${slug}` : `/team/${slug}`}
-      className={clsx(
-        'rounded-md flex items-center justify-center h-12 w-[calc(100%-1rem)] mb-3 mx-[0.5rem] font-semibold border-2 border-color-1',
-        manage ? 'text-0' : 'text-opposite bg-opposite',
-      )}
-    >
-      {t(manage ? 'common.manage.team' : 'common.public.team')}
-    </Link>
+    <SegmentedButton
+      className="mx-3 mb-4"
+      initialIndex={manage ? 0 : 1}
+      options={[
+        { label: 'Vue publique', action: `/team/${slug}` },
+        { label: 'Vue gestion', action: `/manage/team/${slug}` },
+      ]}
+    />
   );
 }
