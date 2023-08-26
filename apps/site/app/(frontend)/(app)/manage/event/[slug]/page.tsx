147b1f0a3caa00a7b77b0ad9bc0f7bd6024aa3b7
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-08-27 00:22:20 +0200

    ✨ Site: event manage page

--- apps/site/app/(frontend)/(app)/manage/event/[slug]/page.tsx
+++ apps/site/app/(frontend)/(app)/manage/event/[slug]/page.tsx
@@ -5,10 +5,14 @@ import BannerImage from '../../../../../../components/atoms/Image/BannerImage';
 import GroupItem from '../../../../../../components/atoms/Item/GroupItem';
 import ViewLayout from '../../../../../../components/atoms/Layout/ViewLayout';
 
-import ActionButton from '../../../../../../components/molecules/Button/ActionButton';
+import LocationForm, { locationSchema } from '../../../../../../components/forms/LocationForm';
+
 import ChangeSetToast from '../../../../../../components/organisms/Form/ChangeSetToast';
+
+import ActionButton from '../../../../../../components/molecules/Button/ActionButton';
 import ImageEditorForm from '../../../../../../components/molecules/ImageEditor/ImageEditorForm';
 import DateInput from '../../../../../../components/molecules/Input/Date/DateInput';
+import FieldSet from '../../../../../../components/molecules/Input/FieldSet';
 import SelectInput from '../../../../../../components/molecules/Input/Select/SelectInput';
 import TextAreaInput from '../../../../../../components/molecules/Input/TextAreaInput';
 import TextInput from '../../../../../../components/molecules/Input/TextInput';
@@ -17,44 +21,45 @@ import { useEventManage, useMe } from '../../../../../../context/navigation';
 import { useModal } from '../../../../../../hooks/context/useModal';
 
 import { BANNER_ASPECT_RATIO } from '@okampus/shared/consts';
-import { Buckets, EntityName, TeamPermissions } from '@okampus/shared/enums';
-import { useUpdateEventMutation } from '@okampus/shared/graphql';
+import { Buckets, EntityName, LocationType, TeamPermissions } from '@okampus/shared/enums';
+import {
+  useInsertAddressMutation,
+  useUpdateEventMutation,
+  useUpdateEventOrganizeProjectManyMutation,
+  useUpdateLocationMutation,
+} from '@okampus/shared/graphql';
 import { ActionType } from '@okampus/shared/types';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import { IconHistory } from '@tabler/icons-react';
 import { Controller, useForm } from 'react-hook-form';
 
 import * as z from 'zod';
 
-import type { EventManageInfo } from '../../../../../../context/navigation';
 import type { LocationMinimalInfo } from '../../../../../../types/features/location.info';
+import type { EventManageInfo } from '../../../../../../utils/apollo/fragments';
 
 function ManageEventPageInner({ eventManage }: { eventManage: EventManageInfo }) {
-  const eventUpdateSchema = z
-    .object({
-      description: z.string().max(10_000, { message: 'La description ne peut pas dépasser 10 000 caractères.' }),
-      name: z
-        .string({ required_error: "Nom de l'événement requis." })
-        .min(3, { message: "Le nom de l'événement doit faire au moins 3 caractères." })
-        .max(100, { message: "Le nom de l'événement ne peut pas dépasser 100 caractères." }),
-      start: z
-        .date({ required_error: 'Date de début requise.' })
-        .min(new Date(), { message: "L'événement ne peut pas commencer dans le passé." }),
-      end: z.date({ required_error: 'Date de fin requise.' }),
-      location: z.any().nullable() as z.ZodType<LocationMinimalInfo | null>,
-      projects: z.record(z.string(), z.string().nullable()).array(),
-    })
-    .refine((data) => data.end > data.start, {
-      message: "La date et heure de fin de l'événement doit être après sa date et heure de début.",
-      path: ['endDate'],
-    });
+  const eventUpdateSchema = z.object({
+    description: z.string().max(10_000, { message: 'La description ne peut pas dépasser 10 000 caractères.' }),
+    name: z
+      .string({ required_error: "Nom de l'événement requis." })
+      .min(3, { message: "Le nom de l'événement doit faire au moins 3 caractères." })
+      .max(100, { message: "Le nom de l'événement ne peut pas dépasser 100 caractères." }),
+    start: z.string({ required_error: 'Date de début requise.' }),
+    end: z.string({ required_error: 'Date de fin requise.' }),
+    projects: z.record(z.string(), z.string().nullable()).array(),
+  });
+  // .refine((data) => new Date(data.end) > new Date(data.start), {
+  //   message: "La date et heure de fin de l'événement doit être après sa date et heure de début.",
+  //   path: ['endDate'],
+  // });
 
   const defaultValues: z.infer<typeof eventUpdateSchema> = {
     name: eventManage.name,
     description: eventManage.description,
-    start: new Date(eventManage.start),
-    end: new Date(eventManage.end),
-    location: eventManage.location ?? null,
+    start: eventManage.start.slice(0, 16),
+    end: eventManage.end.slice(0, 16),
     projects: eventManage.eventOrganizes.map(({ team, project }) => ({ [team.id]: project?.id ?? null })),
   };
 
@@ -73,9 +78,10 @@ function ManageEventPageInner({ eventManage }: { eventManage: EventManageInfo })
       ),
   );
 
+  const [insertAddress] = useInsertAddressMutation();
   const [updateEvent] = useUpdateEventMutation();
-  // const [updateLocation] = useUpdateLocationMutation();
-  // const [updateEventOrganizeProjectMany] = useUpdateEventOrganizeProjectManyMutation();
+  const [updateEventOrganizeProjectMany] = useUpdateEventOrganizeProjectManyMutation();
+  const [updateLocation] = useUpdateLocationMutation();
 
   const updateBanner = () =>
     openModal({
@@ -97,70 +103,122 @@ function ManageEventPageInner({ eventManage }: { eventManage: EventManageInfo })
       ),
     });
 
-  const { handleSubmit, register, formState, reset, control } = useForm({
+  const { handleSubmit, register, formState, reset, control, watch } = useForm({
     defaultValues,
     resolver: zodResolver(eventUpdateSchema),
   });
 
-  const onSubmit = handleSubmit((update) => {
-    updateEvent({
-      variables: {
-        update: {
-          name: update.name,
-          description: update.description,
-          end: update.end.toISOString(),
-          start: update.start.toISOString(),
-        },
-        id: eventManage.id,
-      },
-      onCompleted: () => reset({}, { keepValues: true }),
+  const locationDefaultValues = {
+    location: {
+      name: eventManage.location.name,
+      type: eventManage.location.type,
+      address: eventManage.location.address,
+      link: eventManage.location.link,
+      details: eventManage.location.details,
+    },
+  };
+
+  const locationForm = useForm<{ location: LocationMinimalInfo }>({
+    defaultValues: locationDefaultValues,
+    resolver: zodResolver(locationSchema),
+  });
+
+  const onSubmitLocation = (
+    addressId: string | null,
+    location: {
+      type: string;
+      details: string;
+      link: string;
+      name: string;
+    },
+  ) => {
+    updateLocation({
+      variables: { id: eventManage.location.id, update: { addressId, ...location } },
+      onCompleted: () => locationForm.reset({}, { keepValues: true }),
     });
+  };
+
+  const onSubmit = handleSubmit((update) => {
+    const { projects, start, end, description, name } = update;
+
+    if (formState.isDirty) {
+      updateEvent({
+        variables: { id: eventManage.id, update: { name, description, end, start } },
+        onCompleted: () =>
+          updateEventOrganizeProjectMany({
+            variables: {
+              updates: Object.entries(projects).map(([teamId, projectId]) => ({
+                where: { eventId: { _eq: eventManage.id }, teamId: { _eq: teamId } },
+                update: { projectId },
+              })),
+            },
+            onCompleted: () => reset({}, { keepValues: true }),
+          }),
+      });
+    }
+
+    if (locationForm.formState.isDirty && location) {
+      const { address, ...locationData } = locationForm.getValues('location');
+
+      // @ts-ignore
+      if (address) delete address.__typename;
+
+      if (locationData.type === LocationType.Online) {
+        onSubmitLocation(null, locationData);
+      } else if (locationData.type === LocationType.Unspecificed) {
+        onSubmitLocation(null, { ...locationData, link: '', name: '' });
+      } else {
+        if (!address) onSubmitLocation(null, locationData);
+        else if ('id' in address && typeof address.id === 'string') onSubmitLocation(address.id, locationData);
+        else
+          insertAddress({
+            variables: { object: address },
+            onCompleted: ({ insertAddressOne }) => onSubmitLocation(insertAddressOne?.id ?? null, locationData),
+          });
+
+        onSubmitLocation(null, locationData);
+      }
+    }
   });
 
   return (
-    <ViewLayout header={`Gérer : ${eventManage.name}`}>
+    <ViewLayout header={`Gérer : ${eventManage.name}`} sidePanelIcon={<IconHistory />}>
       {eventManage && (
-        <form onSubmit={onSubmit} className="grid lg-max:grid-cols-1 lg:grid-cols-[auto_1fr] gap-x-16">
+        <form onSubmit={onSubmit} className="grid lg-max:grid-cols-1 lg:grid-cols-[19.5rem_1fr] gap-x-12">
           <ChangeSetToast
-            isDirty={formState.isDirty}
-            isValid={formState.isValid}
-            isLoading={formState.isSubmitting}
-            onCancel={() => reset(defaultValues)}
+            isDirty={formState.isDirty || locationForm.formState.isDirty}
+            isValid={Object.entries(formState.errors).length === 0 && locationForm.formState.isValid}
+            onCancel={() => {
+              reset(defaultValues);
+              locationForm.reset(locationDefaultValues);
+            }}
           />
-          <GroupItem heading="Bannière">
-            <span className="flex flex-col gap-4">
+          <span className="flex flex-col gap-4">
+            <GroupItem heading="Bannière">
               <span className="relative grow overflow-hidden" style={{ aspectRatio: BANNER_ASPECT_RATIO }}>
-                <BannerImage src={eventManage.banner?.url} />
+                <BannerImage className="rounded-xl" src={eventManage.banner?.url} />
                 <div
                   onClick={updateBanner}
-                  className="p-5 absolute -inset-px opacity-0 hover:opacity-50 outline outline-black outline-1 z-20 cursor-pointer bg-black text-white flex gap-1 items-center justify-center"
+                  className="rounded-2xl p-5 absolute -inset-px opacity-0 hover:opacity-50 outline outline-black outline-1 z-20 cursor-pointer bg-black text-white flex gap-1 items-center justify-center"
                 >
                   <div className="font-semibold text-center">Changer de bannière</div>
                 </div>
               </span>
-              <div className="shrink-0 flex justify-between py-1.5">
+            </GroupItem>
+            <div className="shrink-0 flex justify-between py-1.5 mb-2.5">
+              <ActionButton
+                action={{ label: 'Changer la bannière', linkOrActionOrMenu: updateBanner, type: ActionType.Primary }}
+              />
+              {eventManage.banner && (
                 <ActionButton
                   action={{
-                    label: 'Changer la bannière',
-                    linkOrActionOrMenu: updateBanner,
-                    type: ActionType.Primary,
+                    label: 'Enlever',
+                    linkOrActionOrMenu: () =>
+                      updateEvent({ variables: { id: eventManage.id, update: { bannerId: null } } }),
                   }}
                 />
-                {eventManage.banner && (
-                  <ActionButton
-                    action={{
-                      label: 'Enlever',
-                      linkOrActionOrMenu: () =>
-                        updateEvent({ variables: { id: eventManage.id, update: { bannerId: null } } }),
-                    }}
-                  />
-                )}
-              </div>
-            </span>
-          </GroupItem>
-          <hr className="border-color-2 my-10 col-[1/-1] hidden lg-max:block" />
-          <div className="flex flex-col gap-4">
-            <TextInput error={formState.errors.name?.message} label="Nom de l'événements" {...register('name')} />
+              )}
+            </div>
             <DateInput
               error={formState.errors.start?.message}
               label="Début de l'événement"
@@ -169,7 +227,7 @@ function ManageEventPageInner({ eventManage }: { eventManage: EventManageInfo })
             />
             <DateInput
               error={formState.errors.end?.message}
-              label="Fin de l'événements"
+              label="Fin de l'événement"
               includeTime={true}
               {...register('end')}
             />
@@ -181,7 +239,10 @@ function ManageEventPageInner({ eventManage }: { eventManage: EventManageInfo })
                   name={`projects.${idx}.${team.id}`}
                   render={({ field }) => (
                     <SelectInput
-                      {...field}
+                      name={field.name}
+                      value={field.value}
+                      error={formState.errors.projects?.[idx]?.[team.id]?.message}
+                      onChange={field.onChange}
                       options={team.projects.map((project) => ({ label: project.name, value: project.id }))}
                       label={`${team.actor.name} - Projet lié`}
                     />
@@ -189,11 +250,22 @@ function ManageEventPageInner({ eventManage }: { eventManage: EventManageInfo })
                 />
               </div>
             ))}
+          </span>
+          <hr className="border-color-2 my-10 col-[1/-1] hidden lg-max:block" />
+          <div className="flex flex-col gap-4">
+            <TextInput error={formState.errors.name?.message} label="Nom de l'événement" {...register('name')} />
+            <FieldSet label="Où a lieu l'événement ?">
+              <LocationForm formMethods={locationForm} />
+            </FieldSet>
           </div>
           <hr className="border-color-2 my-10 col-[1/-1]" />
-          <GroupItem heading="Présentation longue" groupClassName="flex flex-col gap-5">
-            <TextAreaInput {...register('description')} label="Description" rows={7} />
-          </GroupItem>
+          <TextAreaInput
+            className="col-[1/-1]"
+            error={formState.errors.description?.message}
+            label="Description"
+            rows={8}
+            {...register('description')}
+          />
         </form>
       )}
     </ViewLayout>
