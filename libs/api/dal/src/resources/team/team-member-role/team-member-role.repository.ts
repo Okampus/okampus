b629d7c94f72870f5bd0c4d2406cee1c97c88741
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-08-27 22:23:52 +0200

    🐛 DAL: export missing entities

--- libs/api/dal/src/resources/team/team-member-role/team-member-role.repository.ts
+++ libs/api/dal/src/resources/team/team-member-role/team-member-role.repository.ts
@@ -1,6 +1,6 @@
 import { BaseRepository } from '../../../shards/abstract/base/base.repository';
 import type { TeamMemberRole } from './team-member-role.entity';
 
-export class TeamMemberRepository extends BaseRepository<TeamMemberRole> {
+export class TeamMemberRoleRepository extends BaseRepository<TeamMemberRole> {
   //
 }
