0dc3263acf9b9775fac82f829ef70dc2b7627e43
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-09-15 03:18:04 +0200

    🔇 API: remove read-s3-file log

--- libs/api/shards/src/utils/read-s3-file.ts
+++ libs/api/shards/src/utils/read-s3-file.ts
@@ -2,7 +2,6 @@ import { GetObjectCommand } from '@aws-sdk/client-s3';
 import type { S3Client } from '@aws-sdk/client-s3';
 
 export async function readS3File(s3Client: S3Client, bucket: string, key: string) {
-  console.log('readS3File', bucket, key);
   const request = new GetObjectCommand({ Bucket: bucket, Key: key });
   try {
     const { Body } = await s3Client.send(request);
@@ -10,8 +9,8 @@ export async function readS3File(s3Client: S3Client, bucket: string, key: string
     if (!byteArray) return null;
 
     return Buffer.from(byteArray);
-  } catch {
-    console.log('readS3File error', bucket, key);
+  } catch (error) {
+    console.error({ error, bucket, key });
     return null;
   }
 }
