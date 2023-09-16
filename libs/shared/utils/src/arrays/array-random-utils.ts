3688f5349f3f900a0dd255a6604ba3780097d1c1
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-09-16 02:00:57 +0200

    ✏️ Utils: update array-random-utils

--- libs/shared/utils/src/arrays/array-random-utils.ts
+++ libs/shared/utils/src/arrays/array-random-utils.ts
@@ -1,16 +1,16 @@
 import { shuffleArray } from './shuffle';
 
-export function randomFromArrayWithRemainder<T>(arr: T[], min: number, max = 0): [output: T[], remainder: T[]] {
+export function pickWithRemainder<T>(arr: T[], min: number, max = 0): [output: T[], remainder: T[]] {
   arr = shuffleArray([...arr]);
   const size = max ? Math.floor(Math.random() * (max - min + 1)) + min : min;
 
   return [arr.slice(0, size), arr.slice(size)];
 }
 
-export function pickOneFromArray<T>(arr: T[]): T {
+export function pickOneRandom<T>(arr: T[]): T {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-export function randomFromArray<T>(arr: T[], min: number, max = 0): T[] {
-  return randomFromArrayWithRemainder<T>(arr, min, max)[0];
+export function pickRandom<T>(arr: T[], min: number, max = 0): T[] {
+  return pickWithRemainder<T>(arr, min, max)[0];
 }
