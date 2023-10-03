f9dfccaa8e8e8cf7c91363020515b4bcd8cf56b3
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-10-03 18:02:46 +0200

    ♻️ ✨ Docker: make hasura docker entrypoint production-friendly

--- apps/site/docker/hasura/entrypoint.sh
+++ apps/site/docker/hasura/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -z "$VERCEL_ACCESS_TOKEN" ]; then echo 'Environment variable VERCEL_ACCESS_TOKEN must be specified. Exiting.'; exit 1; fi
+if [ -z "$VERCEL_PROJECT_ID" ]; then echo 'Environment variable VERCEL_PROJECT_ID must be specified. Exiting.'; exit 1; fi
+if [ -z "$VERCEL_TEAM_ID" ]; then echo 'Environment variable VERCEL_TEAM_ID must be specified. Exiting.'; exit 1; fi
+if [ -z "$NAMESPACE" ]; then echo 'Environment variable NAMESPACE must be specified. Exiting.'; exit 1; fi
+if [ "$NAMESPACE" != "default" ] && [ "$NAMESPACE" != "preprod" ]; then echo 'Environment variable NAMESPACE must be either default or preprod. Exiting.'; exit 1; fi
+
 # Declare an associative array of namespace
 declare -A kubernetes_namespace
 
@@ -9,11 +15,14 @@ kubernetes_namespace[preprod]=development
 
 # Get env from vercel
 json_data=$(curl -X GET "https://api.vercel.com/v7/projects/$VERCEL_PROJECT_ID/env?decrypt=true&source=vercel-cli:pull&teamId=$VERCEL_TEAM_ID" -H "Authorization: Bearer $VERCEL_ACCESS_TOKEN")
-processed=$(echo $json_data | grep -Poh "\"envs\":\[(.*)\]")
-envs=(${processed//"},{"/ })
-namespace="${kubernetes_namespace[$NAMESPACE]}"
 
-echo $json_data
+# If json_data contains "Project not found", exit
+if [[ $json_data == *"Project not found"* ]]; then echo "VERCEL_PROJECT_ID is incorrect: Project not found. Exiting."; exit 1; fi
+if [[ $json_data == *"Not authorized"* ]]; then echo "VERCEL_ACCESS_TOKEN or VERCEL_TEAM_ID is incorrect: Not authorized. Exiting."; exit 1; fi
+
+processed=$(echo $json_data | grep -Poh "\"envs\":\[(.*)\]") # get the envs array
+envs=(${processed//"},{"/ }) # split by "},{"
+namespace="${kubernetes_namespace[$NAMESPACE]}" # get the namespace from associative array
 
 declare -A env_map
 for env in "${envs[@]}"
@@ -26,6 +35,16 @@ do
   fi
 done
 
+if [ -z "${env_map["PSQL_USER"]}" ]; then echo 'Environment variable PSQL_USER must be set in Vercel. Exiting.'; exit 1; fi
+if [ -z "${env_map["PSQL_PASSWORD"]}" ]; then echo 'Environment variable PSQL_PASSWORD must be set in Vercel. Exiting.'; exit 1; fi
+if [ -z "${env_map["PSQL_HOST"]}" ]; then echo 'Environment variable PSQL_HOST must be set in Vercel. Exiting.'; exit 1; fi
+if [ -z "${env_map["POSTGRES_PUBLISHED_PORT"]}" ]; then echo 'Environment variable POSTGRES_PUBLISHED_PORT must be set in Vercel. Exiting.'; exit 1; fi
+if [ -z "${env_map["POSTGRES_DB"]}" ]; then echo 'Environment variable POSTGRES_DB must be set in Vercel. Exiting.'; exit 1; fi
+if [ -z "${env_map["JWT_ALGORITHM"]}" ]; then echo 'Environment variable JWT_ALGORITHM must be set in Vercel. Exiting.'; exit 1; fi
+if [ -z "${env_map["ACCESS_TOKEN_SECRET"]}" ]; then echo 'Environment variable ACCESS_TOKEN_SECRET must be set in Vercel. Exiting.'; exit 1; fi
+if [ -z "${env_map["HASURA_GRAPHQL_ADMIN_SECRET"]}" ]; then echo 'Environment variable HASURA_GRAPHQL_ADMIN_SECRET must be set in Vercel. Exiting.'; exit 1; fi
+if [ -z "${env_map["HASURA_INPUT_VALIDATION_WEBHOOK_URL"]}" ]; then echo 'Environment variable HASURA_INPUT_VALIDATION_WEBHOOK_URL must be set in Vercel. Exiting.'; exit 1; fi
+
 export HASURA_GRAPHQL_DATABASE_URL=postgres://${env_map["PSQL_USER"]}:${env_map["PSQL_PASSWORD"]}@${env_map["PSQL_HOST"]}:${env_map["POSTGRES_PUBLISHED_PORT"]}/${env_map["POSTGRES_DB"]}
 export HASURA_GRAPHQL_JWT_SECRET=\{\"type\":\"${env_map["JWT_ALGORITHM"]}\",\"key\":\"${env_map["ACCESS_TOKEN_SECRET"]}\",\"claims_namespace\":\"https://hasura.io/jwt/claims\",\"claims_format\":\"json\",\"header\":\{\"type\":\"Cookie\",\"name\":\"OKAMPUS_ACCESS_TOKEN\"\}\}
 export HASURA_GRAPHQL_ADMIN_SECRET=${env_map["HASURA_GRAPHQL_ADMIN_SECRET"]}
@@ -34,7 +53,5 @@ export HASURA_INPUT_VALIDATION_WEBHOOK_URL=${env_map["HASURA_INPUT_VALIDATION_WE
 unset VERCEL_ACCESS_TOKEN
 unset VERCEL_PROJECT_ID
 unset VERCEL_TEAM_ID
-
-printenv
 
 /bin/graphql-engine serve
