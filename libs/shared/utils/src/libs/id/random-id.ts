e68baddd6dacd4f2b24945bf9c3aae4b879cea80
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-10-09 20:03:43 +0200

    🐛 Utils: limit randomId's alphabet

--- libs/shared/utils/src/libs/id/random-id.ts
+++ libs/shared/utils/src/libs/id/random-id.ts
@@ -1,4 +1,4 @@
-export function randomId(length = 21, dictionary = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!+') {
+export function randomId(length = 21, dictionary = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
   // eslint-disable-next-line unicorn/prefer-node-protocol
   const random = typeof self === 'undefined' ? require('crypto').getRandomValues : self.crypto.getRandomValues;
   const randomBytes = random(new Uint8Array(length));
