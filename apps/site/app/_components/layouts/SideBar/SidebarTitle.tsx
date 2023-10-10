f7d9cdbb1aa7e0748e9338fed0509f22ede75ad2
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-10-10 11:03:11 +0200

    💄 🐛 SidebarTitle: fix line clamp

--- apps/site/app/_components/layouts/SideBar/SidebarTitle.tsx
+++ apps/site/app/_components/layouts/SideBar/SidebarTitle.tsx
@@ -1,11 +1,19 @@
 import clsx from 'clsx';
 
 export type SideBarTitleProps = { children: React.ReactNode; className?: string; separator?: boolean };
-export default function SideBarTitle({ children, className, separator = true }: SideBarTitleProps) {
+export default function SideBarTitle({ children, className = 'text-0', separator = true }: SideBarTitleProps) {
   const titleClassName = clsx(
     className,
-    'px-4 w-full line-clamp-1 text-0 font-semibold text-xl flex items-center h-[var(--h-topbar)] shrink-0',
-    separator && 'border-b border-[var(--border-2)] mb-3',
+    'font-semibold text-ellipsis text-xl whitespace-nowrap overflow-hidden min-w-0',
   );
-  return <div className={titleClassName}>{children}</div>;
+  return (
+    <div
+      className={clsx(
+        'px-4 w-full flex items-center shrink-0 h-[var(--h-topbar)]',
+        separator && 'border-b border-[var(--border-2)] mb-3',
+      )}
+    >
+      <div className={titleClassName}>{children}</div>
+    </div>
+  );
 }
