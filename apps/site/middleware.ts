22542c42cb303baa9159f1af6dd15cbba1b1a1a0
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-10-11 22:25:10 +0200

    🐛 Site: ignore api paths in locale middleware

--- apps/site/middleware.ts
+++ apps/site/middleware.ts
@@ -15,16 +15,7 @@ export async function middleware(request: NextRequest) {
   // Redirect if there is no locale
   const locale = await getLang(request.headers.get('accept-language'), request.cookies.get(LOCALE_COOKIE)?.value);
   request.nextUrl.pathname = `/${locale}${pathname}`;
-  // e.g. incoming request is /products
-  // The new URL is now /en-US/products
   return Response.redirect(request.nextUrl);
 }
 
-export const config = {
-  matcher: [
-    // Skip all internal paths (_next)
-    '/((?!_next).*)',
-    // Optional: only run on root (/) URL
-    // '/'
-  ],
-};
+export const config = { matcher: ['/((?!_next|api).*)'] };
