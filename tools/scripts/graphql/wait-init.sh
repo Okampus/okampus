66b695866c4999227a44ae042e6dad0fd0adb230
Ivan STEPANIAN <iv.stpn@gmail.com>
2023-10-16 10:05:47 +0200

    🐛 Scripts: fix wait init for hasura
    Co-Authored-By: noftaly <noftaly@gmail.com>

--- tools/scripts/graphql/wait-init.sh
+++ tools/scripts/graphql/wait-init.sh
@@ -1,4 +1,7 @@
-until nc -z -w5 $HASURA_HOST 8080; do
+hostname=$(echo $NEXT_PUBLIC_HASURA_FQDN | cut -d ':' -f 1)
+port=$(echo $NEXT_PUBLIC_HASURA_FQDN | cut -d ':' -f 2)
+
+until nc -z -w5 $hostname $port; do
   echo "Waiting for hasura to be up..."
   sleep 1
 done
